f899090e6e16c0c71f1584f61de589e3bad18476
MikloCO <37591806+MikloCO@users.noreply.github.com>
2019-04-18 15:04:29 +0200

    Added churchdoor prefab

---
+++ Unity-projekt/SPM-Grupp05-VT19/Assets/Footsteps.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Footsteps : MonoBehaviour
+{
+    //RunState playerScript;
+
+    // Start is called before the first frame update
+    private AudioSource PlayerSource;
+    public AudioClip runStepsMarble;
+    void Start()
+    {
+        //    playerScript = GetComponent<RunState>();
+        PlayerSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift) && !PlayerSource.isPlaying) 
+        {
+            PlayerSource.Play(0);
+        }
+    }
+}
