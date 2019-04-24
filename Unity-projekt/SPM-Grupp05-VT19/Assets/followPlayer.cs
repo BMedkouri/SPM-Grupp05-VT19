a2be417183bbb4e1b2786caeef1b7146f9fb1585
MikloCO <37591806+MikloCO@users.noreply.github.com>
2019-04-24 13:02:09 +0200

    Checkpoints, enemyfollow
    -Added enemyfollow script
    -Added checkpoints

--- Unity-projekt/SPM-Grupp05-VT19/Assets/followPlayer.cs
+++ Unity-projekt/SPM-Grupp05-VT19/Assets/followPlayer.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class followPlayer : MonoBehaviour
 {
-    public float speed = 2.0f;
-    public float accuracy = 0.01f;
+    Enemy enemy;
+
     public Transform goal;
+    public float speed = 2.0f;
+    public float accuracy = 1.0f;
+    public float rotSpeed = 0.4f;
+
+    Collider col;
 
     void Start()
     {
@@ -16,12 +21,23 @@ public class followPlayer : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
-        Vector3 lookAtGoal = new Vector3(goal.position.x, this.transform.position.y,
-            goal.position.z);
-        this.transform.LookAt(lookAtGoal);
+        
+        
 
-        if (Vector3.Distance(transform.position, lookAtGoal) > accuracy)
-            this.transform.Translate(0, 0, speed * Time.deltaTime);
+     
+            Vector3 lookAtGoal = new Vector3(goal.position.x, this.transform.position.y,
+                goal.position.z);
 
+            Vector3 direction = lookAtGoal - this.transform.position;
+
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation,
+                                                        Quaternion.LookRotation(direction),
+                                                        Time.deltaTime * rotSpeed);
+            if (Vector3.Distance(transform.position, lookAtGoal) > accuracy)
+                this.transform.Translate(0, 0, speed * Time.deltaTime);
+        
+
+       
     }
+ 
 }
