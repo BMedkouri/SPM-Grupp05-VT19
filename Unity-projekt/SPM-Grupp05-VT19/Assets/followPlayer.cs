b60161ad39ab2811731cc960296071bb882f54b4
MikloCO <37591806+MikloCO@users.noreply.github.com>
2019-04-24 10:38:26 +0200

    Fixed whitebox lvl1
    Added AI which follows player
    Added fences

---
+++ Unity-projekt/SPM-Grupp05-VT19/Assets/followPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followPlayer : MonoBehaviour
+{
+    public float speed = 2.0f;
+    public float accuracy = 0.01f;
+    public Transform goal;
+
+    void Start()
+    {
+        this.transform.LookAt(goal.position);
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 lookAtGoal = new Vector3(goal.position.x, this.transform.position.y,
+            goal.position.z);
+        this.transform.LookAt(lookAtGoal);
+
+        if (Vector3.Distance(transform.position, lookAtGoal) > accuracy)
+            this.transform.Translate(0, 0, speed * Time.deltaTime);
+
+    }
+}
