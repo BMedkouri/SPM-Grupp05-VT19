c4e9fefcfe7775ee4c9e10acff96e1a39db494e2
Stella C <37660246+Nicaisce@users.noreply.github.com>
2019-04-26 10:58:35 +0200

    Lore Function start
    Lore Function start

---
+++ Unity-projekt/SPM-Grupp05-VT19/Assets/Scripts/UI/LorePopupTextScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LorePopupTextScript : MonoBehaviour
+{
+
+    public Canvas loreCanvas;
+
+    private bool isVisible;
+
+    public Image blackFade;
+    public Text[] loreTextList;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       /* loreCanvas = GetComponent<Canvas>();
+        blackFade = GetComponent<Image>();
+        loreTextList = GetComponents<Text>(); */
+
+        loreCanvas.enabled = true;
+
+        blackFade.enabled = false;
+        isVisible = false;
+    }
+    
+    public void OnClick()
+    {
+   
+
+        if (Input.GetKeyDown("Fire1"))
+        {
+            if(isVisible == false)
+            {
+                blackFade.enabled = true;
+                isVisible = true;
+            }
+        }
+        else
+        {
+            blackFade.enabled = false;
+            isVisible = false;
+        }
+      
+
+        Debug.Log("ButtonClick");
+    }
+
+}
