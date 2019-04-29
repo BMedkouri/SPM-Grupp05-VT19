cfda468bdb7dde5ea251c62c78d5f04abc6d9d91
Stella C <37660246+Nicaisce@users.noreply.github.com>
2019-04-29 08:53:09 +0200

    Main Menu attempt
    Main menu attempt added

---
+++ Unity-projekt/SPM-Grupp05-VT19/Assets/Scripts/UI/ExitButtonScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExitButtonScript : MonoBehaviour
+{
+
+    public void ClickQuitGame()
+    {
+
+        Application.Quit();
+
+        Debug.Log("Quitting game");
+    }
+}
---
+++ Unity-projekt/SPM-Grupp05-VT19/Assets/Scripts/UI/StartGameSctipy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGameSctipy : MonoBehaviour
+{
+
+    [SerializeField] private string loadLevel;
+
+    void Start()
+    {        
+    }
+
+    void Update()
+    {   
+    }
+
+
+    public void OnClickStartGame()
+    {
+        SceneManager.LoadScene(loadLevel);
+    }
+}
