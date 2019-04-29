e795f2c62707f4273218fc62b53608e2e0bb29e0
MikloCO <37591806+MikloCO@users.noreply.github.com>
2019-04-29 08:48:37 +0200

    Fix ups
    -Delete bridgegate if u walk into
    -change between scenes

---
+++ Unity-projekt/SPM-Grupp05-VT19/Assets/Scripts/Change.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Change : MonoBehaviour
+{
+	[SerializeField] private string loadLevel;
+//	public Animator animator;
+
+	void OnTriggerEnter(Collider other) {
+		if(other.CompareTag("Player")) {
+			SceneManager.LoadScene(loadLevel);
+		
+		}
+	}
+    /*
+	public void FadeToLevel(int levelIndex) {
+		animator.SetTrigger("FadeOut");
+	}
+    */
+}
