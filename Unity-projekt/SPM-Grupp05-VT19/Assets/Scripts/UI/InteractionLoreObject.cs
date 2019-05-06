3450ea7a50daaf35be97a29ae6423c19d9032970
MikloCO <37591806+MikloCO@users.noreply.github.com>
2019-05-06 13:32:42 +0200

    Added new GUI for lore

---
+++ Unity-projekt/SPM-Grupp05-VT19/Assets/Scripts/UI/InteractionLoreObject.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InteractionLoreObject : MonoBehaviour
+{
+    public string myString;
+    public Text myText;
+    public float fadeTime;
+    public bool displayInfo;
+  //  public Button myButton;
+
+    public GameObject myButton1;
+   
+    void Start()
+    {
+        myText = GameObject.Find("Text").GetComponent<Text>();
+        myText.color = Color.clear;
+
+       // myButton = GetComponent<Button>();
+        myButton1.SetActive(false);
+       
+    }
+        
+    void Update()
+    {
+        FadeText();
+    }
+
+    void OnMouseOver()
+    {
+        {
+            displayInfo = true;
+            //myButton.enabled = true;
+
+            myButton1.SetActive(true);
+
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        displayInfo = false;
+        myButton1.SetActive(false);
+    }
+
+    void FadeText()
+    {
+        if(displayInfo)
+        {
+            myText.text = myString;
+            myText.color = Color.Lerp(myText.color, Color.white, fadeTime * Time.deltaTime);
+        }
+
+        else
+        {
+            myText.color = Color.Lerp(myText.color, Color.clear, fadeTime * Time.deltaTime);
+            
+        }
+    }
+}
---
+++ Unity-projekt/SPM-Grupp05-VT19/Assets/UI/UI Lore/Scripts/LoreDialouge.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class LoreDialouge : MonoBehaviour
+{
+
+    public TextMeshProUGUI textDisplay;
+    public string[] sentences;
+    private int index;
+    public float typingSpeed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Type());
+    }
+
+    
+    
+    IEnumerator Type()
+    {
+        foreach(char letter in sentences[index].ToCharArray())
+        {
+            textDisplay.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+
+    public void NextSentance()
+    {
+        if(index < sentences.Length - 1)
+        {
+            index++;
+            textDisplay.text = "";
+            StartCoroutine(Type());
+        }
+        else
+        {
+            textDisplay.text = "";
+        }
+    }
+    
+}
