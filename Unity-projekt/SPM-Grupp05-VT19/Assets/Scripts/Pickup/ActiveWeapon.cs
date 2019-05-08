0d596389e38c01859b9b63ee75fb8c96485fba37
BMedkouri <Bilal.Medkouri@gmail.com>
2019-05-08 10:21:37 +0200

    10:19 - 08-05-2019
    - Spelaren har nu olika vapen, och spelarens attacker kollar nu efter vapnets skada >och< attackens skada.

---
+++ Unity-projekt/SPM-Grupp05-VT19/Assets/Scripts/Pickup/ActiveWeapon.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveWeapon : MonoBehaviour
+{
+    // Active weapon
+    [SerializeField] private GameObject activeWeapon;
+
+    public GameObject GetActiveWeapon()
+    {
+        return activeWeapon;
+    }
+
+    public void SetActiveWeapon(GameObject weapon)
+    {
+        activeWeapon = weapon;
+    }
+
+    public string GetActiveWeaponName()
+    {
+        return activeWeapon.GetComponent<Weapon>().GetWeaponName();
+    }
+
+    public float GetActiveWeaponDamage()
+    {
+        return activeWeapon.GetComponent<Weapon>().GetWeaponDamage();
+    }
+}
---
+++ Unity-projekt/SPM-Grupp05-VT19/Assets/Scripts/Pickup/Weapon.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] private string weaponName;
+    [SerializeField] private float weaponDamage;
+    // Add possible trail effect and particle effect (in form of a glow(?))
+
+    public string GetWeaponName()
+    {
+        return weaponName;
+    }
+
+    public float GetWeaponDamage()
+    {
+        return weaponDamage;
+    }
+}
