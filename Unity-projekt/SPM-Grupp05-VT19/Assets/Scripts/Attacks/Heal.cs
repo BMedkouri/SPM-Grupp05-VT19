7aad924cd680e52b05a4e8c1eb65ee42a5c3f32b
RagnarAnders <37663886+RagnarAnders@users.noreply.github.com>
2019-05-09 09:00:12 +0200

    Påbörjat en heal

---
+++ Unity-projekt/SPM-Grupp05-VT19/Assets/Scripts/Attacks/Heal.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Heal : MonoBehaviour
+{
+
+    [SerializeField] protected string healName;
+    [SerializeField] protected float healAmount;
+    [SerializeField] protected AudioSource soundEffect;
+    [SerializeField] protected ParticleSystem particleEffect;
+
+    public void TriggerParticleEvent()
+    {
+        ParticleEvent particleEvent = new ParticleEvent
+        {
+            ParticleLocation = transform.position,
+            ParticleSystem = particleEffect
+        };
+        particleEvent.FireEvent();
+    }
+
+    public void TriggerSoundEvent()
+    {
+        SoundEvent soundEvent = new SoundEvent
+        {
+            AudioLocation = transform.position,
+            AudioSource = soundEffect
+        };
+        soundEvent.FireEvent();
+    }
+
+    public void HealObject()
+    {
+        GameObject.FindGameObjectWithTag("Player").GetComponent<HealthComponent>().RecoverHealth(healAmount);
+    }
+}
