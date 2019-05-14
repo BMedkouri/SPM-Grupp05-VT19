ed527a7d377ac45d909a8a874e2ea4aea05e76b5
MikloCO <37591806+MikloCO@users.noreply.github.com>
2019-05-14 12:21:56 +0200

    La tillbaka filer som försvann efter merge

---
+++ Unity-projekt/SPM-Grupp05-VT19/Assets/Scripts/PrefabHandlers/ParticlePrefabHandler.cs
@@ -0,0 +1,16 @@
+﻿//Author: Bilal El Medkouri
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticlePrefabHandler : MonoBehaviour
+{
+    private void Update()
+    {
+        if (!GetComponent<ParticleSystem>().IsAlive())
+        {
+            Destroy(gameObject);
+        }
+    }
+}
