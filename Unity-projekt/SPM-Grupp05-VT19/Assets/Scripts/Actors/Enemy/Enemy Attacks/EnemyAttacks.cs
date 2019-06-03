298e46b28307573448c174f80261865904839b4c
RagnarAnders <37663886+RagnarAnders@users.noreply.github.com>
2019-06-03 14:13:56 +0200

    fienden så färdiga de kan bli innan test
    fienden så färdiga de kan bli innan test

--- Unity-projekt/SPM-Grupp05-VT19/Assets/Scripts/Actors/Enemy/Enemy Attacks/EnemyAttacks.cs
+++ Unity-projekt/SPM-Grupp05-VT19/Assets/Scripts/Actors/Enemy/Enemy Attacks/EnemyAttacks.cs
@@ -5,12 +5,27 @@ using UnityEngine;
 public class EnemyAttacks : MonoBehaviour
 {
     [SerializeField] private float damage;
+    [SerializeField] private ParticleSystem particleSystem;
+
+    private void Awake()
+    {
+        particleSystem.Play();
+        particleSystem.emissionRate = 0f;
+    }
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.collider.CompareTag("Player"))
+        if (collision.collider.CompareTag("Offhand"))
+        {
+            GetComponentInParent<Animator>().SetTrigger("Parried");
+        }
+        else if (collision.collider.CompareTag("Player"))
         {
             DamageEvent damageEvent = new DamageEvent(damage, gameObject, collision.collider.gameObject);
             damageEvent.FireEvent();
+
+            ParticleEvent particle = new ParticleEvent(transform.position, particleSystem);
+            particle.FireEvent();
+            particleSystem.emissionRate = 1f;
         }
     }
 }
