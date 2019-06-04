2aae259b61381e46ecf0b1a554647eaeb9914593
MalleB <malin_borggren@hotmail.com>
2019-06-04 10:13:29 +0200

    particle effects

--- Unity-projekt/SPM-Grupp05-VT19/Assets/Scripts/InstantiateAoe.cs
+++ Unity-projekt/SPM-Grupp05-VT19/Assets/Scripts/InstantiateAoe.cs
@@ -6,14 +6,24 @@ public class InstantiateAoe : MonoBehaviour
 {
     [SerializeField] private GameObject Aoe;
     [SerializeField] private ParticleSystem particleSystem;
+    GameObject attack;
     public void InstantiateAOE()
     {
+        Debug.Log(transform.tag + " Enemy tag");
         if (particleSystem != null)
         {
             ParticleEvent particle = new ParticleEvent(transform.position, particleSystem);
             particle.FireEvent();
         }
-        GameObject attack = Instantiate(Aoe, transform);
-        Destroy(attack, 0.1f);
+        if(transform.tag == "Enemy")
+        {
+
+            attack = Instantiate(Aoe, transform.position, Quaternion.Euler(-90f,0f,0f));
+        }
+        else
+        {
+            attack = Instantiate(Aoe, transform);
+        }
+        //Destroy(attack, 0f);
     }
 }
